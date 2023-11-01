d7913b745e19d6feaab6336c2b21c24f4052735c
Robert Blindt <117112044+robertblindt@users.noreply.github.com>
2023-11-01 11:22:45 -0400

    change home card background image from the overlay to CSS to make resizing less janky. Image is just duplicated 4x making the wrap a bit jarring though.

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,5 +1,4 @@
 import Card from 'react-bootstrap/Card'
-import image1 from "./homecard.jpg";
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom'
 
@@ -27,38 +26,34 @@ export default function HomeCard({isLoggedIn}: HomeCardProps) {
         <>
         <h4 className='ws-font pt-3 text-center fw-light'>This website is currently hosted using a few different free services, so if you request a card and don't get a response, give it 30 or 40 seconds and try again.</h4>
         <h4 className='ws-font text-center fw-light'>These free services turn off when not in use and take a bit to turn back on!</h4>
-        <Card className='my-3 text-center border border-dark border-2 rounded-3 grad high-card-shadow ws-font overflow-auto'>
-            <Card.Img src={image1} alt="Card image"/>
-            <Card.ImgOverlay className='grad-home'>
-                <Card.Title className='fs-1 fw-normal'>Where do you start when a new Commander catches your eye?</Card.Title>
-                <Card.Subtitle className='mt-4 fs-2 fw-normal'>CommanderGrams is here to help!</Card.Subtitle>
-                {/* <Card.Subtitle className='mt-3 fs-3 fw-normal'>Or even more daunting, what if you are building your first Commander deck?! </Card.Subtitle> */}
-                <Card.Body className='fs-4 mt-3'>
-                    
-                    Not many people can keep track of the over 33,000 unique Magic the Gathering cards from the games over 30 years!  Right now there are more being release just about every month! The MTG community is lucky enough to have some tech-savvy players that have been gracious enough to create databases and website that track what is actively being released and built!
-                    <br/>
-                    <br/>
-                    When you request a Commander below, we check EDHrecs recommendations for cards and let you know what we see in common with them.
-                    <br/>
-                    <br/>
-                    We also have a deck building tool with some enhanced features!  You can set up search terms for your deck in order to track how many times an effect occurs in your deck! Never wonder how many cards 'do the thing' again!
-                    <br/>
-                    { isLoggedIn ? (
-                        <Button variant='success' className='my-3 w-50' onClick={toBuildDeck}>Build a Deck!</Button>
-                    ):(
-                        <div className='my-3'>
-                        <div className='row'>
-                            <div className='col-6'>
-                            <Button variant='danger' className='w-100' onClick={toRegister}>Register</Button>
-                            </div> 
-                            <div className='col-6'>
-                            <Button variant='primary' className='w-100' onClick={toLogin}>Login</Button>
-                            </div>
+        <Card className='my-3 text-center border border-dark border-2 rounded-3 grad high-card-shadow ws-font overflow-auto hc-bg'>
+            <Card.Title className='fs-1 fw-normal'>Where do you start when a new Commander catches your eye?</Card.Title>
+            <Card.Subtitle className='mt-4 fs-2 fw-normal'>CommanderGrams is here to help!</Card.Subtitle>
+            <Card.Body className='fs-4 mt-3'>
+                
+                Not many people can keep track of the over 33,000 unique Magic the Gathering cards from the games over 30 years!  Right now there are more being release just about every month! The MTG community is lucky enough to have some tech-savvy players that have been gracious enough to create databases and website that track what is actively being released and built!
+                <br/>
+                <br/>
+                When you request a Commander below, we check EDHrecs recommendations for cards and let you know what we see in common with them.
+                <br/>
+                <br/>
+                We also have a deck building tool with some enhanced features!  You can set up search terms for your deck in order to track how many times an effect occurs in your deck! Never wonder how many cards 'do the thing' again!
+                <br/>
+                { isLoggedIn ? (
+                    <Button variant='success' className='my-3 w-50' onClick={toBuildDeck}>Build a Deck!</Button>
+                ):(
+                    <div className='my-3'>
+                    <div className='row'>
+                        <div className='col-6'>
+                        <Button variant='danger' className='w-100' onClick={toRegister}>Register</Button>
+                        </div> 
+                        <div className='col-6'>
+                        <Button variant='primary' className='w-100' onClick={toLogin}>Login</Button>
                         </div>
-                        </div>
-                    ) }
-                </Card.Body>
-            </Card.ImgOverlay>
+                    </div>
+                    </div>
+                ) }
+            </Card.Body>
         </Card>
         </>
     )
